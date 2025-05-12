341c65d7fd8c3750e61eb4bcc741c6be974285a8
tilavjont <tilavjontolibov@icloud.com>
2025-05-12 11:55:12 -0400

    finalize part b

--- Q1/PartB.java
+++ Q1/PartB.java
@@ -1,1 +1,11 @@
-
+public int dogWalkShift(int startHour, int endHour) {
+  int total = 0;
+  for (int i = startHour; i <= endHour; i++) {
+    int dogs = walkDogs(i);
+    total += dogs*5;
+      if (dogs == maxDogs || (i >= 9 && i <= 17)) {
+        total += 3;
+      }
+    }
+      return total;
+}
