c7991ec7825a0c3698a6dade311977af6a3b7211
tilavjont <tilavjontolibov@icloud.com>
2025-05-15 11:35:24 -0400

    Update PartA.java

--- Q1/PartA.java
+++ Q1/PartA.java
@@ -1,1 +1,14 @@
+public int walkDogs(int hour) {
 
+    // find out from DogWalkCompany how many dogs at hour
+    int numDogs = company.numAvailableDogs(hour);
+
+    // if walker can walk more (or equal) dogs than available
+    if (maxDogs >= numDogs) {
+        company.updateDogs(hour, numDogs);
+        return numDogs;
+    } else {
+        company.updateDogs(hour, numDogs);
+        return maxDogs;
+    }
+}
