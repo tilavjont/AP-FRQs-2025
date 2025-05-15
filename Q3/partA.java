59dc38e097d7291a95e1c858208a808b2b16e329
tilavjont <tilavjontolibov@icloud.com>
2025-05-15 11:51:43 -0400

    Update partA.java

--- Q3/partA.java
+++ Q3/partA.java
@@ -1,1 +1,7 @@
+    public Round (String [] names) {
+        for (int i = 0; i < names.length; i++) {
+            Competitor player = new Competitor(names[i], i+1);
+            competitorList.add(player);
+        }
+    }
 
