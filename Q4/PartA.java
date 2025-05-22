a8dcfcbbe908abe274555984d8b7aa0bb88a1840
tilavjont <tilavjontolibov@icloud.com>
2025-05-22 11:40:00 -0400

    Update PartA.java

--- Q4/PartA.java
+++ Q4/PartA.java
@@ -1,8 +1,7 @@
 public sumOrSumGame(int numRows, int numCols) {
-  int arr[][] = new int[numRows][numCols];
   for (int i = 0; i < numRows; i++) {
     for (int j = 0; j < numCols; j++) {
-      arr[i][j] = ((int)(Math.Random()*9 + 1);
+      puzzle = ((int)(Math.Random()*9 + 1);
     }
   }
 
