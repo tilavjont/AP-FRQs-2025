88b08910f49b7d973b7bcc78d5b48f2570fc0a90
tilavjont <tilavjontolibov@icloud.com>
2025-05-22 11:47:04 -0400

    Update PartB.java

--- Q4/PartB.java
+++ Q4/PartB.java
@@ -1,2 +1,14 @@
 public boolean clearPair(int row, int col) {
-  for (int i = 0; i<arr.length
+  for (int i = 0; i < puzzle.length; i++) {
+    for (int j = 0; j < puzzle[0].length; j++) {
+      if ((i >= row && col != j) && (puzzle[i][j] == puzzle[row][col] || puzzle[i][j] + puzzle[row][col] == 10)) {
+        puzzle[i][j] == 0;
+        puzzle[row][col] == 0;
+        return true;
+      }
+    return false;
+    }
+  }
+}
+      
+      
